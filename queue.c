1ad32cf62e9f9314eb6aa6958dd750d9ecb606f3
Dennis Liu <dennis48161025@gmail.com>
2025-02-24 16:53:49 +0800

    Implement traversal-based element count
    In this commit, 'q_size' was implemented. Traverse the queue elements
    using the next pointer. For each list member whose address is not equal
    to the head's address, increment the element count. Continue traversal
    until the head is encountered again. The function has a time complexity
    of O(n).
    
    Change-Id: I20d1b92830cf576435f6f040efda5e725924f463

--- queue.c
+++ queue.c
@@ -9,7 +9,7 @@ struct list_head *q_new()
 {
     struct list_head *p = malloc(sizeof(struct list_head));
 
-    if (NULL == p)
+    if (!p)
         return p;
 
     INIT_LIST_HEAD(p);
@@ -110,7 +110,15 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    if (!head || list_empty(head))
+        return 0;
+
+    int size = 0;
+    struct list_head *node;
+    list_for_each (node, head)
+        ++size;
+
+    return size;
 }
 
 /* Delete the middle node in queue */
