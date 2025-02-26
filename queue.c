29de02e18faa545ebdca6234b7a7d5b4db186309
Dennis Liu <dennis48161025@gmail.com>
2025-02-26 08:26:16 +0800

    Implement 'q_reverse'
    This commit implements 'q_reverse'. Treat the 'tail' as the new list
    head, and insert all nodes into new head sequentially from original head
    to tail using head insertion. Stop when a node reaches the tail.
    
    Change-Id: Id1de58cf8525e69c1fca6c388716c85cdcc194cd

--- queue.c
+++ queue.c
@@ -182,7 +182,20 @@ void q_swap(struct list_head *head)
 }
 
 /* Reverse elements in queue */
-void q_reverse(struct list_head *head) {}
+void q_reverse(struct list_head *head)
+{
+    if (!head || list_empty(head) || list_is_singular(head))
+        return;
+
+    const struct list_head *tail = head->prev;
+    struct list_head *node, *safe;
+    list_for_each_safe (node, safe, head) {
+        // move current element to tail->next
+        if (node == tail)
+            return;
+        list_move(node, head);
+    }
+}
 
 /* Reverse the nodes of the list k at a time */
 void q_reverseK(struct list_head *head, int k)
